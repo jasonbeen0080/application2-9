7b60b79c20967424507672ad325302cc7c0a8c39
jasonbeen0080 <iamnotzurabutkatsura@gmail.com>
2021-09-12 07:35:42 +0000

    application2-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2b7aedeb181b9f8e739b2c7a4d538866305761de93037ad6c4e66c6823b0a96f4a3c7763f19ef788506e29c7f886fe2f859d38b557101b6042a2fc4be143170a'
